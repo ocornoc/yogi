009b32de1b948c91ffcaa07362a014576d1c26c2
ocornoc <ocornoc@protonmail.com>
2021-07-18 09:05:42 -0700

    add parsing entire script from str

--- src/main.rs
+++ src/main.rs
@@ -6,56 +6,20 @@ mod vm;
 mod arith;
 
 fn script() -> parser::raw::Script {
-    let mut script = parser::raw::Script::default();
-    script.0[0].0 =
-        ":done++ b=97 c=89"
-        .chars()
-        .collect();
-    script.0[1].0 =
-        ":o++ :done++"
-        .chars()
-        .collect();
-    script.0[2].0 =
-        ":done++ x-- x=\"abc\" x=atan x"
-        .chars()
-        .collect();
-    script.0[3].0 =
-        "i=(127-1) _=(i/3%1==0)*i/3>1+(i/5%1==0)*i/5>1+(i/7%1==0)*i/7>1 a=i/11%1==0 x=atan x"
-        .chars()
-        .collect();
-    script.0[4].0 =
-        "_+=a*i/11>1+(i/13%1==0)*i/13>1+(i/17%1==0)*i/17>1+(i/19%1==0)*i/19>1 x=atan x"
-        .chars()
-        .collect();
-    script.0[5].0 =
-        "_+=(i/23%1==0)*i/23>1+(i/29%1==0)*i/29>1+(i/31%1==0)*i/31>1a=i/37%1==0 x=atan x"
-        .chars()
-        .collect();
-    script.0[6].0 =
-        "_+=a*i/37>1+(i/41%1==0)*i/41>1+(i/43%1==0)*i/43>1+(i/47%1==0)*i/47>1 x=atan x"
-        .chars()
-        .collect();
-    script.0[7].0 =
-        "_+=(i/53%1==0)*i/53>1+(i/59%1==0)*i/59>1+(i/61%1==0)*i/61>1a=i/67%1==0 x=atan x"
-        .chars()
-        .collect();
-    script.0[8].0 =
-        "_+=a*i/67>1+(i/71%1==0)*i/71>1+(i/73%1==0)*i/73>1+(i/79%1==0)*i/79>1 x=atan x"
-        .chars()
-        .collect();
-    script.0[9].0 =
-        "_+=(i/83%1==0)*i/83>1+(i/c%1==0)*i/c>1+(i/b%1==0)*i/b>1:o+=_<1:done++ x=atan x"
-        .chars()
-        .collect();
-    script.0[10].0 =
-        "a=1 if _ then a=2 else a=\"2\" end _/=a"
-        .chars()
-        .collect();
-    script.0[11].0 =
-        "z=:o :done++goto 4"
-        .chars()
-        .collect();
-    script
+    "\
+    :done++ b=97 c=89\n\
+    :o++ :done++\n\
+    :done++ x-- x=\"abc\" x=atan x\n\
+    i=(127-1) _=(i/3%1==0)*i/3>1+(i/5%1==0)*i/5>1+(i/7%1==0)*i/7>1 a=i/11%1==0 x=atan x\n\
+    _+=a*i/11>1+(i/13%1==0)*i/13>1+(i/17%1==0)*i/17>1+(i/19%1==0)*i/19>1 x=atan x\n\
+    _+=(i/23%1==0)*i/23>1+(i/29%1==0)*i/29>1+(i/31%1==0)*i/31>1a=i/37%1==0 x=atan x\n\
+    _+=a*i/37>1+(i/41%1==0)*i/41>1+(i/43%1==0)*i/43>1+(i/47%1==0)*i/47>1 x=atan x\n\
+    _+=(i/53%1==0)*i/53>1+(i/59%1==0)*i/59>1+(i/61%1==0)*i/61>1a=i/67%1==0 x=atan x\n\
+    _+=a*i/67>1+(i/71%1==0)*i/71>1+(i/73%1==0)*i/73>1+(i/79%1==0)*i/79>1 x=atan x\n\
+    _+=(i/83%1==0)*i/83>1+(i/c%1==0)*i/c>1+(i/b%1==0)*i/b>1:o+=_<1:done++ x=atan x\n\
+    a=1 if _ then a=2 else a=\"2\" end _/=a\n\
+    z=:o :done++goto 4\n\
+    ".parse().unwrap()
 }
 
 fn set_core_affinity() {
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -17,6 +17,38 @@ pub mod raw {
     #[derive(Debug, Default)]
     pub struct Line(pub Vec<char>);
 
+    #[derive(Debug, Clone, Copy, Error)]
+    pub enum ParseErr {
+        #[error("Too many lines in the script")]
+        TooManyLines,
+        #[error("A line was too long")]
+        LineTooLong,
+        #[error("The script wasn't ASCII")]
+        NonAscii,
+    }
+
     #[derive(Debug, Default)]
     pub struct Script(pub [Line; NUM_LINES]);
+
+    impl FromStr for Script {
+        type Err = ParseErr;
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            if !s.is_ascii() {
+                return Err(ParseErr::NonAscii);
+            }
+            let mut script = Self::default();
+            for (i, line) in s.lines().enumerate() {
+                if i >= NUM_LINES {
+                    return Err(ParseErr::TooManyLines);
+                }
+                let line = line.chars().collect::<Vec<_>>();
+                if line.len() >= LINE_LENGTH {
+                    return Err(ParseErr::LineTooLong);
+                }
+                script.0[i] = Line(line);
+            }
+            Ok(script)
+        }
+    }
 }
